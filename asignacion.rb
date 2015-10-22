61619b9c0b5bae234d201d73ea87a1bee875f69e
aziria1 <azrodar@gmail.com>
2015-10-22 12:40:27 -0400

    Update asignacion.rb

--- asignacion.rb
+++ asignacion.rb
@@ -3,4 +3,4 @@ puts "Table of Content".center( line_width )
 puts "=" * 48
 puts "Chapter 1\: Get Started".ljust( line_width) + "Page 1".rjust( 0)
 puts "Chapter 2\: All about cats".ljust( line_width) + "Page 3".rjust(0)
-puts "Chapter 3\: The parts of the bicycle".ljust( line_width ) + "Page 4".rjust(0-6)
+puts "Chapter 3\: The parts of the bicycle".ljust( line_width ) + "Page 4".rjust(0)
